c2ca20bbb9d48e33a7cc46e075b786a617659f05
amanuelgthn <amanuelgthn@gmail.com>
2024-02-04 13:01:14 +0100

    places.py documentation

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""Place Blueprint"""
+"""
+Place blueprint
+"""
+
+
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
